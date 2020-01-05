bef44b770b1ae658fa5fc4629289b7bc3254b145
lowell.rueca <lowellrueca@gmail.com>
2020-01-05 09:01:22 +0800

    serving exception module out from route dir

---
+++ app/exceptions.py
@@ -0,0 +1,22 @@
+"""
+This modules contains application exception handlers
+"""
+from app.views import template_env, template
+
+
+async def error_403(request, exception):
+    page = template_env.get_template('error_403.html')
+    context = {'request': request}
+    return template.TemplateResponse(page, context=context, status_code=400)
+
+
+async def error_404(request, exception):
+    page = template_env.get_template('error_404.html')
+    context = {'request': request}
+    return template.TemplateResponse(page, context=context, status_code=400)
+
+
+async def error_500(request, exception):
+    page = template_env.get_template('error_500.html')
+    context = {'request': request}
+    return template.TemplateResponse(page, context=context, status_code=500)
