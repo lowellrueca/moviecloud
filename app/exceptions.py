9424e05a9ef3c6dcf749c816af057e1fab575a94
lowell.rueca <lowellrueca@gmail.com>
2020-01-14 22:12:08 +0800

    implement error 400 and fixing typo error

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,19 +1,28 @@
 """
 This modules contains application exception handlers
 """
+from starlette.routing import Route
+from starlette.responses import HTMLResponse
+from starlette.exceptions import HTTPException
 from app.resources import template_env, template
+
+
+async def error_400(request, exception):
+    page = template_env.get_template('error_400.html')
+    context = {'request': request}
+    return template.TemplateResponse(page, context=context, status_code=400)
 
 
 async def error_403(request, exception):
     page = template_env.get_template('error_403.html')
     context = {'request': request}
-    return template.TemplateResponse(page, context=context, status_code=400)
+    return template.TemplateResponse(page, context=context, status_code=403)
 
 
 async def error_404(request, exception):
     page = template_env.get_template('error_404.html')
     context = {'request': request}
-    return template.TemplateResponse(page, context=context, status_code=400)
+    return template.TemplateResponse(page, context=context, status_code=404)
 
 
 async def error_500(request, exception):
