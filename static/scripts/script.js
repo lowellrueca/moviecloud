5c3ca1ef98ee1c4f174e69c47f06df6f1b8bb77e
lowell.rueca <lowellrueca@gmail.com>
2020-02-03 22:54:49 +0800

    implement header event scripts

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,18 +1,31 @@
-// header and drop navigation function
-window.addEventListener('DOMContentLoaded', () => {
-    // toggle drop navigation function
-    let headerMenu = document.getElementById('header-menu');
-    headerMenu.addEventListener('click', () => {
-        let dropNav = document.getElementById('drop-nav');
-        let classToggle = 'drop-nav--is-off';
-        dropNav.classList.toggle(classToggle);
-    });
+// Handles types of events for the header html element
+const headerEventHandler = () => {
+    let buttonClick = (buttonElementId, toggleElement, toggleClass) => {
+        let buttonElement = document.getElementById(buttonElementId);
+        buttonElement.addEventListener('click', () => {
+            let elem = document.getElementById(toggleElement);
+            elem.classList.toggle(toggleClass);
+        });
+    }
 
-    // toggle function for drop movie category
-    let dropMovie = document.getElementById('drop-nav-movies-btn');
-    dropMovie.addEventListener('click', () => {
-        let dropMovieCategories = document.getElementById('drop-nav-movies-category');
-        let classToggle = 'drop-nav__li-drop__ul--is-off';
-        dropMovieCategories.classList.toggle(classToggle);
-    });
-});
+    buttonClick('movie-drop-btn', 'movie-drop-menu', 'header-div-nav__drop__ul--is-off');
+    buttonClick('header-menu', 'drop-nav', 'drop-nav--is-off');
+    buttonClick('drop-nav-movies-btn', 'drop-nav-movies-category', 'drop-nav__li-drop__ul--is-off');
+
+    this.addEventListener('resize', headerResizeEventHandler);
+}
+
+const headerResizeEventHandler = () => {
+    let resetDropElement = (dropElementId, addClass) => {
+        let elem = document.getElementById(dropElementId);
+        if(window.getComputedStyle(elem).display !== 'none'){
+            elem.classList.add(addClass);
+        }
+    }
+
+    resetDropElement('movie-drop-menu', 'header-div-nav__drop__ul--is-off');
+    resetDropElement('drop-nav', 'drop-nav--is-off');
+    resetDropElement('drop-nav-movies-category', 'drop-nav__li-drop__ul--is-off');
+}
+
+window.addEventListener('DOMContentLoaded', headerEventHandler);
